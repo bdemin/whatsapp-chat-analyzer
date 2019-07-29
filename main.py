ec0ef915ea13dc71b4ac9b6e3d1cab26df519f33
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-29 23:09:20 +0300

    Added data parser

--- main.py
+++ main.py
@@ -31,10 +31,33 @@ def get_message_data(line):
     
     message = ' '.join(split_line[1:])
     
-    if is_author(message): # Split message from author
-        split_message = message.split(': ')
+    if is_author(message):
+        split_message = message.split(': ') # Split message from author
         author = split_message[0]
         message = ' '.join(split_message[1:])
     else:
         author = None
     return date, time, author, message
+
+
+def parse_data(path):
+    parsed_data = []
+    with open(path, encoding = "utf-8") as fp:
+        message_buffer = [] # buffer for capturing long messages
+        date, time, author = None, None, None # Set default
+        
+        while True:
+            line = fp.readline()
+            if not line: # Reached EOF
+                break
+            line = line.strip() # clean line from whitespaces
+            if is_datetime_pattern(line): # Beginning of a new message
+                if len(message_buffer) > 0: # Add message data from previous line, if needed
+                    parsed_data.append([date, time, author, ' '.join(message_buffer)]) # Save message data
+                message_buffer = []
+                date, time, author, message = get_message_data(line)
+                message_buffer.append(message)
+            else:
+                message_buffer.append(line) # Continue message from previous lines
+    print('Parsing done')
+    return parsed_data
