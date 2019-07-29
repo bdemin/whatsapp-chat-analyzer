e1414b5bc425cbc7a5755c756d775a72efb1721d
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-29 22:33:56 +0300

    Added regex handling to match WhatsApp data

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import re
+
+
+def is_datetime_pattern(s):
+    pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]):([0-9][0-9]) -'
+    result = re.match(pattern, s)
+    if result:
+        return True
+    return False
