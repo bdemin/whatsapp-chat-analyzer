7ea7c9704a76a79ac1fc31c392c753e5236b4d71
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-29 22:40:07 +0300

    Add message author regex handling

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ import re
 
 
 def is_datetime_pattern(s):
+    # mm/dd/yyyy, hh:mm -
     pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]):([0-9][0-9]) -'
+    result = re.match(pattern, s)
+    if result:
+        return True
+    return False
+
+def is_author(s):
+    patterns = [
+        '([\w]+):',                        # First Name
+        '([\w]+[\s]+[\w]+):',              # First Name + Last Name
+        '([\w]+[\s]+[\w]+[\s]+[\w]+):',    # First Name + Middle Name + Last Name
+    ]
+    pattern = '^' + '|'.join(patterns)
     result = re.match(pattern, s)
     if result:
         return True
